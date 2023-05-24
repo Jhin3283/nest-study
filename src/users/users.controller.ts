20dcb2db5541e8b74368a0fe8ae427accc042648
jhin3283 <hj3283@gmail.com>
2023-05-24 15:47:43 +0900

    STUDY: Controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,16 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query,Res, HttpStatus } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserLoginDto } from './dto/user-login.dto';
 import { VerifyEmailDto } from './dto/verify-email.dto';
+import { Response } from 'express';
 // import { UserInfo } from './UserInfo';
 
 @Controller('users')
 export class UsersController {
-  @Post()
-  async createUser(@Body() dto: CreateUserDto): Promise<void> {
+  @Post('/')
+  async createUser(@Body() dto: CreateUserDto, @Res() res: Response): Promise<Response> {
     console.log(dto);
+    return res.status(HttpStatus.CREATED).send();
   }
 
   @Post('/email-verify')

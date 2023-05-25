0ef29ffadfbb2c9a21f2f414b2e554fef4410c9a
jhin3283 <hj3283@gmail.com>
2023-05-25 10:10:25 +0900

    ADD: UserInfo

--- src/users/UserInfo.ts
+++ src/users/UserInfo.ts
@@ -1,6 +1,12 @@
+import { User } from './user.interface';
+
 export class UserInfo {
-    id: number;
-    name: string;
-    email: string;
-    password: string;
-  }
+  private users: User[] = [
+    {
+      id: 1,
+      name: 'Dexter',
+      email: 'dexter.haan@gmail.com',
+      password: 'PASSWORD',
+    },
+  ];
+}

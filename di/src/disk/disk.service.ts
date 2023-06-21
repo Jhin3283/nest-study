290d346745da5c4bd2fa558f5ec8017684bf0125
jhin3283 <hj3283@gmail.com>
2023-06-21 11:17:12 +0900

    [ADD] di

--- di/src/disk/disk.service.ts
+++ di/src/disk/disk.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
-
+import { PowerService } from 'src/power/power.service';
 @Injectable()
-export class DiskService {}
+export class DiskService {
+  constructor(private poswerService: PowerService) {}
+  getData() {
+    console.log('Drawing 20 watts of power from PowerService');
+    this.poswerService.supplyPower(20);
+    return 'data!';
+  }
+}
